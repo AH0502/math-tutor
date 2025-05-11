9a4a89b4e6175169f0575686ddb0dba2312f51eb
Alex Hagopian <hagopianalexander@gmail.com>
2025-05-10 20:01:43 -0400

    Add .pdf support

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st 
 from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.schema import HumanMessage
 from PIL import Image
 import io
 from base64 import b64encode
+import tempfile
+
 
 
 st.title("🧑‍🏫 Math Tutor ")
@@ -15,7 +18,7 @@ with st.form("main"):
     text = st.text_area("Enter tutor prompt:")
     submitted = st.form_submit_button()
 
-if file is not None and submitted:
+if file is not None and submitted and file.type != 'application/pdf':
     image = Image.open(file)
     buffered = io.BytesIO()
     image.save(buffered, format="JPEG")
@@ -38,9 +41,31 @@ if file is not None and submitted:
         response = llm.invoke(messages)
         st.success("Response:")
         st.write(response.content)
+elif file is not None and file.type == 'application/pdf':
 
+    with tempfile.NamedTemporaryFile(delete=False, suffix="pdf") as tmp_file:
+        tmp_file.write(file.read())
+        tmp_path = tmp_file.name
 
+    loader = PyMuPDFLoader(tmp_path)
 
+    pages = loader.load()
 
+    pdf_data = "\n".join([page.page_content for page in pages])
 
+    llm = ChatOpenAI(
+    model='gpt-4o',
+    temperature=0
+    )
+
+    messages = [
+        HumanMessage(content=[
+            {"type": "text", "text": text},
+            {"type": "text", "text": pdf_data}
+        ])
+    ]
+    with st.spinner("Thinking"):
+        response = llm.invoke(messages)
+        st.success("Response:")
+        st.write(response.content)
 
