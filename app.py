090b96ee18e92c650c83b756cd44ca21911bc24f
Alex Hagopian <hagopianalexander@gmail.com>
2025-05-11 18:10:23 -0400

    Refactor code

--- app.py
+++ app.py
@@ -1,71 +1,32 @@
 import streamlit as st 
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain.schema import HumanMessage
-from PIL import Image
-import io
-from base64 import b64encode
-import tempfile
-
+import src.utils
 
 
 st.title("🧑‍🏫 Math Tutor ")
 
-# Need to make form field here to enter API Key
 
 with st.form("main"):   
+    api_key = st.text_input("Enter your OpenAI API Key:", type="password")
     file = st.file_uploader("Upload notes or homework:", type=['jpg', 'jpeg', 'png', 'pdf'])
     text = st.text_area("Enter tutor prompt:")
     submitted = st.form_submit_button()
 
 if file is not None and submitted and file.type != 'application/pdf':
-    image = Image.open(file)
-    buffered = io.BytesIO()
-    image.save(buffered, format="JPEG")
-    img_b64 = b64encode(buffered.getvalue()).decode()
-    
 
-    llm = ChatOpenAI(
-        model='gpt-4o',
-        temperature=0
-    )
-
-    messages = [
-        HumanMessage(content=[
-        {"type": "text", "text": text},
-        {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{img_b64}"}}  
-    ])
-    ]
+    llm = src.utils.get_llm(api_key=api_key)
 
     with st.spinner("Thinking"):
-        response = llm.invoke(messages)
+        response = src.utils.process_jpeg(file, text, llm)
         st.success("Response:")
-        st.write(response.content)
+        st.markdown(response.content)
+
+
 elif file is not None and file.type == 'application/pdf':
 
-    with tempfile.NamedTemporaryFile(delete=False, suffix="pdf") as tmp_file:
-        tmp_file.write(file.read())
-        tmp_path = tmp_file.name
+    llm = src.utils.get_llm(api_key)
 
-    loader = PyMuPDFLoader(tmp_path)
-
-    pages = loader.load()
-
-    pdf_data = "\n".join([page.page_content for page in pages])
-
-    llm = ChatOpenAI(
-    model='gpt-4o',
-    temperature=0
-    )
-
-    messages = [
-        HumanMessage(content=[
-            {"type": "text", "text": text},
-            {"type": "text", "text": pdf_data}
-        ])
-    ]
     with st.spinner("Thinking"):
-        response = llm.invoke(messages)
+        response = src.utils.process_pdf(file, text, llm)
         st.success("Response:")
-        st.write(response.content)
-
+        st.markdown(response.content, unsafe_allow_html=True)
+        st.code(response.content)
---
+++ src/utils.py
@@ -0,0 +1,54 @@
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.tools.e2b_data_analysis.tool import UploadedFile
+import io
+from base64 import b64encode
+from PIL import Image
+import tempfile
+
+
+def get_llm(api_key: str):
+    return ChatOpenAI(
+        model="gpt-4o",
+        temperature=0,
+        api_key=api_key
+    )
+
+def process_jpeg(file, text, llm:ChatOpenAI):
+    image = Image.open(file)
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    img_b64 = b64encode(buffered.getvalue()).decode()
+
+    messages = [
+        SystemMessage(content=( "You are a helpful math tutor. Your output must be in markdown.")),
+
+        HumanMessage(content=[
+        {"type": "text", "text": text},
+        {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{img_b64}"}}  
+        ])
+
+    ]
+    return llm.invoke(messages)
+
+def process_pdf(file: UploadedFile, text: str, llm: ChatOpenAI):
+        
+    with tempfile.NamedTemporaryFile(delete=False, suffix="pdf") as tmp_file:
+        tmp_file.write(file.read())
+        tmp_path = tmp_file.name
+
+    loader = PyMuPDFLoader(tmp_path)
+
+    pages = loader.load()
+
+    pdf_data = "\n".join([page.page_content for page in pages])
+
+    messages = [
+        HumanMessage(content=[
+            {"type": "text", "text": text},
+            {"type": "text", "text": pdf_data}
+        ])
+    ]
+
+    return llm.invoke(messages)
